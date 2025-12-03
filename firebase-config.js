7d1850b98d3d4a126d788295db79aa1b252dcd16
MYWKSKR39 <158294452+MYWKSKR39@users.noreply.github.com>
2025-12-03 21:30:15 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCoYt0CThkE6uPKODEDwh0FNH3vA_VADsI",
+  authDomain: "smartasset-d18c0.firebaseapp.com",
+  projectId: "smartasset-d18c0",
+  storageBucket: "smartasset-d18c0.firebasestorage.app",
+  messagingSenderId: "345539034912",
+  appId: "1:345539034912:web:e8f8a36e317f37d090e145"
+};
